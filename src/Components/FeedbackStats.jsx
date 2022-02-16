77b749f502888ddf8028b412fe9183e82fa122f0
chanaku <chani7777777@gmail.com>
2022-02-16 08:16:52 +0200

    Update FeedbackStats.jsx

--- src/Components/FeedbackStats.jsx
+++ src/Components/FeedbackStats.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from "prop-types"
 
-function FeedbackStats() {
+function FeedbackStats({ feedback }) {
 //   calculating rating avg
   let average = feedback.reduce((acc,cur)=> {
    return acc+cur.rating
